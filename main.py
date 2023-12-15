8366274758478e324a35ed25ef1a6bf48a5f270e
yyyagiiz <153312938+yyyagiiz@users.noreply.github.com>
2023-12-15 21:32:18 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+import time
+karakterler = "+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+sifre_uzunlugu = int(input('Şifre uzunluğunu giriniz:\n'))
+sifre = ""
+
+for i in range(sifre_uzunlugu):
+    sifre += random.choice(karakterler)
+
+print(sifre)
